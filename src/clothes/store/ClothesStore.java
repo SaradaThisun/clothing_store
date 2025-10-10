f6eaf1dbf3705c6d4169dbf6c9f01495be4c300c
Sarada Thisun <saradathisun06@gmail.com>
2025-10-10 08:03:25 +0530

    Update ClothesStore.java

--- src/clothes/store/ClothesStore.java
+++ src/clothes/store/ClothesStore.java
@@ -7,7 +7,7 @@ import Controller.login_controller;
 public class ClothesStore {
 
     public static void main(String[] args) {
-        // Optional: make the UI look modern
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -19,12 +19,13 @@ public class ClothesStore {
             e.printStackTrace();
         }
 
-        //  Create MVC components
+        //  Create MVC 
         login_view view = new login_view();
         login_model model = new login_model();
         login_controller controller = new login_controller(view, model);
 
-        // Show the login window
+        // Show the login 
         java.awt.EventQueue.invokeLater(() -> view.setVisible(true));
     }
 }
+
